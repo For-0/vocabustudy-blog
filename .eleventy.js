00c7adb4ab2653326186e9075b4fe261c3ee8c3d
Siddhant <115331356+grimsteel@users.noreply.github.com>
2023-01-19 15:35:19 -0600

    noindex the images

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,12 @@
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const pluginRSS = require("@11ty/eleventy-plugin-rss");
 const {DateTime} = require("luxon"); // Luxon is an eleventy dependency
+const EleventyImage = require("@11ty/eleventy-img");
+
+const imgOptions = {
+    urlPath: "/external-images/",
+    formats: ["webp"]
+};
 
 module.exports = eleventyConfig => {
     eleventyConfig.addWatchTarget("./src/scss");
@@ -11,7 +17,19 @@ module.exports = eleventyConfig => {
         return `<small class="is-pulled-right">Posted on <time datetime="${date.toISO()}">${date.toLocaleString({year: '2-digit', month: 'numeric', day: 'numeric', hour: 'numeric', minute: '2-digit', })}</time></small>`;
     });
     eleventyConfig.addJavaScriptFunction("contentSnippet", value => value.replace(/<[^>]*>/g, "").substring(0, 150));
-    eleventyConfig.addJavaScriptFunction("authorPFP", value => `<figure class="media-left"><a href="https://github.com/${value.login}"><p class="image is-48x48"><img class="is-rounded" src="${value.avatar_url}"/></p></a></figure>`);
+    eleventyConfig.addJavaScriptFunction("authorPFP", value => {
+        authorPfp: async pfpUrl => {
+            EleventyImage(pfpUrl, imgOptions);
+            let imageHtml = EleventyImage.generateHTML(EleventyImage.statsSync(pfpUrl, imgOptions), {
+                class: "is-rounded",
+                alt: "Profile Picture",
+                loading: "lazy",
+                decoding: "async"
+            });
+            return 
+        }
+        return `<figure class="media-left"><a href="https://github.com/${value.login}"><p class="image is-48x48">${imageHtml}</p></a></figure>`;
+    });
     eleventyConfig.addJavaScriptFunction("getNewestCollectionItemDate", pluginRSS.getNewestCollectionItemDate);
     eleventyConfig.addJavaScriptFunction("dateToRfc3339", pluginRSS.dateToRfc3339);
     eleventyConfig.addJavaScriptFunction("absoluteUrl", pluginRSS.absoluteUrl);
